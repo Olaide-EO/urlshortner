e1e564ce208e91e10467fb0b8ea57f2993d5e260
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-01 12:19:07 +0100

    add .env gile to gitignore

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://ebenezer:ebenezer@ebenezer-m15jl.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URL);
 app.use("/public",express.static(__dirname + "/public"));
 
 
